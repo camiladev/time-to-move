a12d8c4cd56042f3b4602afd4fa2a3036893bd31
Camila Matos <camila.mila148@gmail.com>
2021-03-04 11:37:47 -0300

    Ajustado layout com responsividade

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,18 +7,30 @@ export function Login(){
     return(
         <div className={style.loginContainer}>
             <section>
-                <h1>TimeToMove</h1>
+                <div className={style.logo}>
+                </div>
+                <div>
+                    <div className={style.login}>
 
-                <strong>Bem-vindo</strong>
-                <p>
-                   <FaGithub /> 
-                    Faça login com seu Github para começar
-                </p>
-                <form>
-                    <input type="text" placeholder="Digite seu username"/>
-                    <button type="button"><FaArrowRight /></button>
+                        <h1>
+                            <img src="logo.png" alt="Logo TimeToMove"/>
+                            TimeToMove
+                        
+                        </h1>
 
-                </form>
+                        <strong>Bem-vindo</strong>
+                        <p>
+                        <FaGithub /> 
+                            Faça login com seu Github para começar
+                        </p>
+                        <form>
+                            <input type="text" placeholder="Digite seu username"/>
+                            <button type="button"><FaArrowRight /></button>
+
+                        </form>
+                    </div>
+
+                </div>
             </section>
 
         </div>
