cefb22b5b95d3f0567d94b6905801d2cb57c4739
Camila Matos <camila.mila148@gmail.com>
2021-03-18 11:26:21 -0300

    Criado componente Ranking iniciado layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from 'next';
  
 import { ChallengesProvider } from '../contexts/ChallengesContext';
 import HomePage from './home';
+import style from '../styles/pages/Index.module.css';
 
 
 interface IndexProps {
@@ -19,7 +20,7 @@ interface IndexProps {
 export default function Home(props: IndexProps) {
   
   return (
-          
+          <div className={style.container}>
               <ChallengesProvider
                 level = {props.level}
                 currentExperience = {props.currentExperience}
@@ -28,6 +29,8 @@ export default function Home(props: IndexProps) {
                 <HomePage />
 
               </ChallengesProvider>
+
+          </div>
 
           
 
--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -7,7 +7,101 @@ export default function Leaderboard(){
     return(
         <div className={styles.container}>
             <h1>Ranking</h1>
-            <p>:hourglass_flowing_sand: Página em construção</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>POSIÇÃO</th> 
+                        <th>USUÁRIO</th>
+                        <th>DESAFIOS</th>
+                        <th>EXPERIÊNCIA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>
+                            <div>
+                                <img src="https://github.com/camiladev.png" alt="{userName}"/>
+                                <div>
+                                    <strong>Camila Matos</strong>
+                                    <p>
+                                        <img src="icons/level.svg" alt="icone level"/>
+                                        Level 1
+                                    </p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p>
+                                5 completados
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                15400 xp
+                            </p>
+                        </td>
+                    </tr>
+
+                    {/* nova linha */}
+
+                    <tr>
+                        <td>1</td>
+                        <td>
+                            
+                            <div>
+                                <img src="https://github.com/camiladev.png" alt="{userName}"/>
+                                <div>
+                                    <strong>Camila Matos</strong>
+                                    <p>
+                                        <img src="icons/level.svg" alt="icone level"/>
+                                        Level 1
+                                    </p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p>
+                                5 completados
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                15400 xp
+                            </p>
+                        </td>
+                    </tr>
+
+                    {/* nova linha */}
+
+                    <tr>
+                        <td>1</td>
+                        <td>
+                            
+                            <div>
+                                <img src="https://github.com/camiladev.png" alt="{userName}"/>
+                                <div>
+                                    <strong>Camila Matos</strong>
+                                    <p>
+                                        <img src="icons/level.svg" alt="icone level"/>
+                                        Level 1
+                                    </p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p>
+                                5 completados
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                15400 xp
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
